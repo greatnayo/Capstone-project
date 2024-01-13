c220710f41039a9e5d2fd4a727da6bf94a502b8c
Ozoani Anayo <greatnayo@gmail.com>
2024-01-13 06:03:03 +0100

    added unit test

--- LittleLemon/restaurant/views.py
+++ LittleLemon/restaurant/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Booking, Menu
+from .serializers import bookingSerializer, menuSerializer
 
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html', {})
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet ):
+    queryset = Booking.objects.all()
+    serializer_class = bookingSerializer
+    permission_classes = [IsAuthenticated]
